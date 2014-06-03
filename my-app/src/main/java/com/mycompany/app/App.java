5cce0cd1aac5c338c8e44769bf279b8365ba20d2
Dan Clark <dan.clark@quanttus.com>
2014-06-03 11:31:13 -0400

    fixed braces

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -4,10 +4,9 @@ package com.mycompany.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public App() {}
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
